4dc0595aa1684df9c79ffb30cdf0bfcd5bfa8d4a
jungtop <tenzintopjor27@gmail.com>
2022-04-03 18:35:22 +0530

    last update

--- dem.py
+++ dem.py
@@ -1,6 +1,8 @@
 import re
-from typing_extensions import dataclass_transform
-
+import csv
+import git
+import github
+from openpecha import github_utils
 def get_err_links():
     new_link = []
     datas = ""
@@ -10,3 +12,22 @@ def get_err_links():
         m = re.match("err: (.*)",data)
         new_link.append(m.group(1))
     return new_link
+
+def get_pecha_names():
+    csv_rows = []
+    with open('pechas_catalogv1.txt','r')as file:
+        csv_rows = file.readlines()
+    return csv_rows
+
+def delete_repo(id):
+    github_utils.delete_repo(id)
+
+if __name__ == "__main__":
+    pecha_ids = get_pecha_names()
+    for pecha_id in pecha_ids:
+        id = pecha_id.replace("\n","")
+        try:
+            delete_repo(id)
+            print(f"deleted {id}")
+        except:
+            print("already deleted")
